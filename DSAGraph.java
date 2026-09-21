75109286f34f2d25b314db8927602c7a11bd7276
tatuhey <17900375@student.curtin.edu.au>
2026-09-21 23:28:33 +0800

    separated graphvertex from graph

--- DSAGraph.java
+++ DSAGraph.java
@@ -1,4 +1,3 @@
-
 public class DSAGraph{
     private DSALinkedList m_vertices;
 
@@ -6,61 +5,28 @@ public class DSAGraph{
         m_vertices = new DSALinkedList();
     }
 
-    public void addVertex(String label, Object value) {
-        m_vertices = new DSAGraphVertex(value, label);
+    // accessor
+    public boolean hasVertex(){
+        boolean result = false;
+        return result;
     }
 
-
-    private class DSAGraphVertex {
-        private Object m_value;
-        private String m_label;
-        private int m_edges;
-        private boolean m_visited;
-        private DSALinkedList m_links;
-        
-        public DSAGraphVertex(Object inValue, String inLabel) {
-            m_value = inValue;
-            m_label = inLabel;
-            m_edges = 0;
-            m_visited = false;
-            m_links = new DSALinkedList();
-
+    public int getVertexCount() {
+        int count = 0;
+        for (Object elem : m_vertices) {
+            
         }
-        
-        // accessor
-        public Object getValue() {
-            return m_value;
-        }
-
-        public String getLabel() {
-            return m_label;
-        }
-
-        public int getEdges() {
-            return m_edges;
-        }
-
-        public boolean getVisited() {
-            return m_visited;
-        }
-
-        public DSALinkedList getAdjacent() {
-            return m_links;
-        }
-
-        @Override 
-        public String toString() {
-            return "Vertex: " + m_label + " | Visited: " + m_visited;
-        }
-
-        // mutator
-        public void setVisited() {
-            m_visited = true;
-        }
-
-        public void clearVisited() {
-            m_visited = false;
-        }
+        return count;
     }
+    
+    // mutator
+    public void addVertex(Object value, String label) {
+        DSAGraphVertex newVertex = new DSAGraphVertex(value, label);
+    }
+
+    public void addEdge(String label1, String label2) {
+        
+    }
+
 
 }
---
+++ DSAGraphVertex.java
@@ -0,0 +1,55 @@
+public class DSAGraphVertex {
+    private Object m_value;
+    private String m_label;
+    private int m_edgeCount;
+    private boolean m_visited;
+    private DSALinkedList m_edges; // edges aka links between vertices
+    
+    public DSAGraphVertex(Object inValue, String inLabel) {
+        m_value = inValue;
+        m_label = inLabel;
+        m_edgeCount = 0;
+        m_visited = false;
+        m_edges = new DSALinkedList();
+    }
+    
+    // accessor
+    public Object getValue() {
+        return m_value;
+    }
+
+    public String getLabel() {
+        return m_label;
+    }
+
+    public int getEdges() {
+        return m_edgeCount;
+    }
+
+    public boolean getVisited() {
+        return m_visited;
+    }
+
+    public DSALinkedList getAdjacent() {
+        return m_edges;
+    }
+
+    @Override 
+    public String toString() {
+        return "Vertex: " + m_label + " | Visited: " + m_visited;
+    }
+
+    // mutator
+    public void setVisited() {
+        m_visited = true;
+    }
+
+    public void clearVisited() {
+        m_visited = false;
+    }
+
+    public void addEdge(DSAGraphVertex vertex) {
+        m_edges.insertLast(vertex);
+        m_edgeCount++;
+    }
+}
