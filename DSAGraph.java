2d38494dde7add35b47ef5856888072dc0346371
raihan-mint <tatuhey@gmail.com>
2026-09-22 13:43:40 +0800

    added mroe methods but still confused

--- DSAGraph.java
+++ DSAGraph.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class DSAGraph{
     private DSALinkedList m_vertices;
 
@@ -6,26 +8,50 @@ public class DSAGraph{
     }
 
     // accessor
-    public boolean hasVertex(){
+    public boolean hasVertex(String label){
+        boolean result = false;
+
+    }
+
+    public int getVertexCount(String label) {
+        int count = 0;
+        return count;
+    }
+
+    public int getEdgeCount(String label) {
+        int count = 0;
+        return count;
+    }
+
+    public DSAGraphVertex getVertex(String label) {
+        
+    }
+
+    public DSALinkedList getAdjacent(String label) {
+
+    }
+    
+    public boolean isAdjacent(String label1, String label2) {
         boolean result = false;
         return result;
     }
 
-    public int getVertexCount() {
-        int count = 0;
-        for (Object elem : m_vertices) {
-            
-        }
-        return count;
+    public void displayAsList() {
+
     }
-    
+
+    public void displayAsMatrix() {
+
+    }
+
     // mutator
     public void addVertex(Object value, String label) {
         DSAGraphVertex newVertex = new DSAGraphVertex(value, label);
+        m_vertices.insertLast(newVertex);
     }
 
     public void addEdge(String label1, String label2) {
-        
+        DSAGraphVertex vertex = 
     }
 
 
